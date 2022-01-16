0910fbfe43cea9681bee47d56fafb6fd0b575a4d
PB <pbaranski@users.noreply.github.com>
2022-01-16 05:57:44 +0100

    Added script for automatic set swagger env url

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -10,4 +10,13 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = () => {
+
+//   // read file and make object
+//   let content = JSON.parse(fs.readFileSync('../../extensions/documentation/config/settings.json ', 'utf8'));
+//   // edit or add property
+//   servers[1].url = `https://${process.env.$heroku-app-name}.herokuapp.com`;
+//   //write file
+//   fs.writeFileSync('file.json', JSON.stringify(content));
+  
+};
