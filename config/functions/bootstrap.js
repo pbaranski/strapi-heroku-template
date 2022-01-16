b19c052227e0691e557fe96c65b27846a0a5a49e
Przemek B <przemek@jaktestowac.pl>
2022-01-16 09:00:50 +0100

    Add adress to swager for heroku

--- config/functions/bootstrap.js
+++ config/functions/bootstrap.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const fs = require("fs");
+const path = require("path");
 /**
  * An asynchronous bootstrap function that runs before
  * your application gets started.
@@ -10,13 +11,35 @@
  * See more details here: https://strapi.io/documentation/v3.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {
+ try {
+        if(process.env.HEROKU_APP_NAME){
+            let settingsPath = path.resolve("extensions", "documentation", "config", "settings.json");
+            // read file and make object
+            let content = JSON.parse(fs.readFileSync(settingsPath, 'utf8'));
+            
+            let url_heroku = `https://${process.env.HEROKU_APP_NAME}.herokuapp.com`;
 
-//   // read file and make object
-//   let content = JSON.parse(fs.readFileSync('../../extensions/documentation/config/settings.json ', 'utf8'));
-//   // edit or add property
-//   servers[1].url = `https://${process.env.$HEROKU_APP_NAME.herokuapp.com`;
-//   //write file
-//   fs.writeFileSync('file.json', JSON.stringify(content));
-  
-};
+            let heroku_server = content.servers.some((val) => {
+                return Object.values(val).includes(url_heroku);
+            })
+
+            console.log(`Heroku server doc url set: ${heroku_server}`);
+
+            if(!heroku_server) {
+                console.log("Updating server settings for documentation");
+                const server = {
+                    url: url_heroku,
+                    description: "Heroku server dev"
+                  };
+                // edit or add property
+                content.servers.push(server);
+                //write file
+                fs.writeFileSync(settingsPath, JSON.stringify(content, null, 2));
+            }
+
+        }
+    } catch (e) {
+      console.error(e);
+    }
+
+module.exports = () => {};
